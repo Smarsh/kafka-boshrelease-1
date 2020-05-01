f6f36fd7b3ad22bd896f1add875d12c2739f4f6a
David <dmidd87@gmail.com>
2020-05-01 12:00:27 -0400

    comment things out for concourse job

--- ci/tasks/create_final_release.sh
+++ ci/tasks/create_final_release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -eux
 
-apt-get install git -y -f > /dev/null
-echo $BOSH_CLIENT_SECRET
+#apt-get install git -y -f > /dev/null
 
 git config --global user.email "dmidd87@gmail.com"
 
@@ -10,5 +9,5 @@ echo "creating final release"
 
 bosh create-release --final --version=2.4.1-1
 
-git add -A
-git commit -m "final release"
+#git add -A
+#git commit -m "final release"
