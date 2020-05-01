8a7755901da8eaa330f1145b87c77aa77d3c5d74
David <dmidd87@gmail.com>
2020-05-01 15:18:27 -0400

    refactoring shell commands into script

--- ci/tasks/create_final_release.sh
+++ ci/tasks/create_final_release.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -eux
 
+echo "Configuring files and directories"
+echo "----"
+mkdir ~/.ssh
+echo "$GITHUB_PRIV_KEY" > ~/.ssh/id_rsa
+git clone $GITHUB_REPO
+export GITHUB_DIR=`echo $GITHUB_REPO | cut -d / -f 5`
+echo "$BOSH_JUMPBOX_KEY" > jumpbox.key
+echo "$BOSH_CA_CERT" > ca_cert.crt
+
+echo "Configuring BOSH environment"
+bosh alias-env $BOSH_ENVIRONMENT -e $BOSH_ENVIRONMENT --ca-cert ${PWD}/ca_cert.crt
+export BOSH_CLIENT=david.middleton@smarsh.com
+export BOSH_ALL_PROXY=ssh+socks5://jumpbox@${BOSH_ENVIRONMENT}:22?private-key=${PWD}/jumpbox.key
+
 apt update
 apt-get install git -y -f > /dev/null
 apt-get install vim -y -f > /dev/null
 
-git config --global user.email "dmidd87@gmail.com"
-
-cd kafka-repo
-
-echo "$BOSH_CLIENT"
+cd $GITHUB_DIR
 
 echo "creating final release"
 
