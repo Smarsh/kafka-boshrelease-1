5a2363da0de21545d258cbd457dc8d99f8055de4
Chinedu Uzoka <acuzoka@gmail.com>
2020-05-27 16:56:38 +0100

    refactor: cleanup or code

--- ci/tasks/create-final-release.sh
+++ ci/tasks/create-final-release.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-set -eux
+set -euo pipefail
 
+SHELL=/bin/bash
 ROOT_DIR=$(pwd)
 
-echo "Configuring files, keys, certs and directories"
-echo "==="
-echo "==="
+if [[ $(echo $TERM | grep -v xterm) ]]; then
+  export TERM=xterm
+fi
+
 GITHUB_REPO="kafka-repo"
-PRERELEASE_REPO=./kafka-prerelease-repo
 BOSH_RELEASE_VERSION=$(cat ${ROOT_DIR}/version/version)
+BOSH_RELEASE_VERSION_FILE=../version/number
+RELEASE_NAME=$(bosh int config/final.yml --path /final_name)
+PRERELEASE_REPO=../kafka-prerelease-repo
+RUN_PIPELINE=0 # if script is running locally then 0 if in consourse pipeline then 1
+
+BOLD=$(tput bold)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+MAGENTA=$(tput setaf 5)
+RESET=$(tput sgr0)
+
+loginfo() {
+  echo
+  echo "###"
+  echo "###"
+  printf "### ${BOLD}${GREEN}${1}${RESET}\n"
+  echo "###"
+  echo
+}
+
+loginfo "Configuring files, keys, certs and directories"
 
 
-echo "$jumpbox_key" | jq -r .private_key > jumpbox.key
-echo "$ca_cert" | jq -r .certificate > ca_cert.crt
 
-echo "Configuring BOSH environment"
-bosh alias-env $BOSH_ENVIRONMENT -e $BOSH_ENVIRONMENT --ca-cert ${PWD}/ca_cert.crt
-export BOSH_ALL_PROXY=ssh+socks5://jumpbox@${BOSH_ENVIRONMENT}:22?private-key=${PWD}/jumpbox.key
+echo "$jumpbox_key" | jq -r .private_key > jumpbox.key > /dev/null
+echo "$ca_cert" | jq -r .certificate > ca_cert.crt > /dev/null
+
+loginfo "Configuring BOSH environment"
+
+bosh alias-env $BOSH_ENVIRONMENT -e $BOSH_ENVIRONMENT --ca-cert ${ROOT_DIR}/ca_cert.crt
+export BOSH_ALL_PROXY=ssh+socks5://jumpbox@${BOSH_ENVIRONMENT}:22?private-key=${ROOT_DIR}/jumpbox.key
 
 ## change directories into the master branch of the repository that is cloned, not the branched clone
 pushd $PRERELEASE_REPO
@@ -24,9 +49,7 @@ pushd $PRERELEASE_REPO
 git config --global user.email "ci@localhost"
 git config --global user.name "CI Bot"
 
-echo "Cutting a final release"
-echo "==="
-echo "==="
+loginfo "Cutting a final release"
 
 ## Download all of the blobs and packages from the kafka-boshrelease bucket that is read only
 
@@ -37,7 +60,7 @@ blobstore:
   provider: s3
   options:
     bucket_name: ${BLOBSTORE}
-name: kafka
+name: ${RELEASE_NAME}
 EOF
 
 ## Create private.yml for BOSH to use our AWS keys
@@ -55,7 +78,7 @@ git update-index --assume-unchanged config/final.yml
 
 git status
 
-bosh create-release --final --version=${BOSH_RELEASE_VERSION} --tarball "../release-tarball/kafka-${BOSH_RELEASE_VERSION}.tgz"
+bosh create-release --final --version=${BOSH_RELEASE_VERSION} --tarball "../release-tarball/${RELEASE_NAME}-${BOSH_RELEASE_VERSION}.tgz"
 
 git status
 git add config .final_builds releases || true
