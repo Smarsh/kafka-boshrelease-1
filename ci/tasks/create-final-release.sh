af244fadb463d87d560ff45652acc0da26f83c49
Chinedu Uzoka <acuzoka@gmail.com>
2020-05-27 16:38:45 +0100

    refactor: cleanup or code

--- ci/tasks/create-final-release.sh
+++ ci/tasks/create-final-release.sh
@@ -54,9 +54,8 @@ git update-index --assume-unchanged config/final.yml
 [[ -d .final_builds ]]  && rm -fr .final_builds
 
 git status
-git commit -am "Pre-stage final release stage change, ${BOSH_RELEASE_VERSION} via concourse"
 
-bosh create-release --final --version=${BOSH_RELEASE_VERSION} --tarball "../s3-rc-release/kafka-${BOSH_RELEASE_VERSION}.tgz"
+bosh create-release --final --version=${BOSH_RELEASE_VERSION} --tarball "../release-tarball/kafka-${BOSH_RELEASE_VERSION}.tgz"
 
 git status
 git add config .final_builds releases || true
